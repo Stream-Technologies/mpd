062355443e48fcfa70d5fc7221a996c0c1b32bef
archiecobbs <archiecobbs>
2002-04-03 20:53:22 +0000

    #include "ppp.h" to get FD_SETSIZE.

--- src/event.c
+++ src/event.c
@@ -6,17 +6,7 @@
  * All rights reserved.
  */
 
-  #include <sys/types.h>
-  #include <sys/param.h>
-  #include <sys/time.h>
-  #include <unistd.h>
-  #include <stdio.h>
-  #include <stdlib.h>
-  #include <signal.h>
-  #include <stdarg.h>
-  #include <string.h>
-  #include <errno.h>
-  #include <err.h>
+  #include "ppp.h"
   #include "event.h"
 
 /*
