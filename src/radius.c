4e59fe9a4cfc0921a06f981421f55dbc8412fd6c
glebius <glebius>
2006-01-15 09:50:24 +0000

    In RadiusGetParams() allocate memory before storing MS domain in there.
    Submitted by:	Arcadiy Ivanov <arcivanov mail.ru>

--- src/radius.c
+++ src/radius.c
@@ -1,7 +1,7 @@
 /*
  * See ``COPYRIGHT.mpd''
  *
- * $Id: radius.c,v 1.28 2005/12/11 10:49:24 glebius Exp $
+ * $Id: radius.c,v 1.29 2005/12/18 11:09:32 glebius Exp $
  *
  */
 
@@ -1167,6 +1167,7 @@ RadiusGetParams(AuthData auth, int eap_proxy)
 	      case RAD_MICROSOFT_MS_CHAP_DOMAIN:
 		Freee(MB_AUTH, a->params.msdomain);
 		tmpval = rad_cvt_string(data, len);
+		a->params.msdomain = Malloc(MB_AUTH, len + 1);
 		memcpy(a->params.msdomain, tmpval, len + 1);
 		free(tmpval);
 		Log(LG_RADIUS, ("[%s] RADIUS: %s: RAD_MICROSOFT_MS_CHAP_DOMAIN: %s",
