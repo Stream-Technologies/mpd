1e9c95fe19cd466c3f5365b65371e453f5c8d5d0
glebius <glebius>
2006-10-13 10:24:42 +0000

    Suppress gcc warning that can be upgraded to error in future.

--- src/radius.c
+++ src/radius.c
@@ -1,7 +1,7 @@
 /*
  * See ``COPYRIGHT.mpd''
  *
- * $Id: radius.c,v 1.43 2006/10/08 19:12:58 amotin Exp $
+ * $Id: radius.c,v 1.44 2006/10/08 22:25:45 amotin Exp $
  *
  */
 
@@ -1234,7 +1234,7 @@ RadiusGetParams(AuthData auth, int eap_proxy)
 	      case RAD_MICROSOFT_MS_CHAP_ERROR:
 		/* there is a nullbyte on the first pos, don't know why */
 		if (((const char *)data)[0] == '\0') {
-		  ((const char *)data)++;
+		  data = (const char *)data + 1;
 		  len--;
 		}
 		tmpval = rad_cvt_string(data, len);
@@ -1253,7 +1253,7 @@ RadiusGetParams(AuthData auth, int eap_proxy)
 		   * Only point at the String field if we don't think the
 		   * peer has misformatted the response.
 		   */
-		  ((const char *)data)++;
+		  data = (const char *)data + 1;
 		  len--;
 		} else
 		  Log(LG_RADIUS, ("[%s] RADIUS: %s: Warning: The MS-CHAP2-Success attribute is mis-formatted. Compensating",
