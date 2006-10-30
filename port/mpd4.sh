727393d6ccdafc8bb62964941d74e3d8699fcd5a
glebius <glebius>
2006-10-30 18:58:31 +0000

    Improve rcNG script.
    Submitted by:	Ilya Pizik <polzun scar.jinr.ru>

--- port/mpd4.sh
+++ port/mpd4.sh
@@ -2,26 +2,44 @@
 #
 # $FreeBSD$
 #
+
 # PROVIDE: mpd
 # REQUIRE: NETWORKING
+# BEFORE: SERVERS
 
 #
 # Add the following lines to /etc/rc.conf to enable mpd4:
+# mpd_enable (bool):	Set to "NO" by default.
+#			Set it to "YES" to enable mpd4.
+# mpd_flags (string):	Set to "-b" by default.
+#			Extra flags passed to start command.
 #
-# mpd_enable="YES"	# YES or NO (default)
+# See mpd(8) for flags.
 #
-mpd_enable=${mpd_enable-"NO"}
 
 . %%RC_SUBR%%
 
-name=mpd4
+name="mpd4"
 rcvar=`set_rcvar mpd`
-prefix=%%PREFIX%%
-pidfile=/var/run/${name}.pid
-command="${prefix}/sbin/${name}"
-command_args="-b -p ${pidfile}"
-required_files="${prefix}/etc/${name}/mpd.conf ${prefix}/etc/${name}/mpd.links"
 
-load_rc_config $name
+load_rc_config ${name}
+
+: ${mpd_enable="NO"}
+: ${mpd_flags="-b"}
+
+pidfile="/var/run/${name}.pid"
+command="%%PREFIX%%/sbin/${name}"
+required_files="%%PREFIX%%/etc/${name}/mpd.conf %%PREFIX%%/etc/${name}/mpd.links"
+
+case "${mpd_flags}" in
+*-p\ *)
+        echo "ERROR: \$mpd_flags includes -p option." \
+                "PID file is already set to $pidfile."
+        exit 1
+        ;;
+*)
+        command_args="${mpd_flags} -p ${pidfile}"
+        ;;
+esac
 
 run_rc_command "$1"
