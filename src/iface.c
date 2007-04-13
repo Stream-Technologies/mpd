daf624494860bdd54fad493eeb853190e74b5aab
amotin <amotin>
2007-04-13 13:33:24 +0000

    minor logging fix

--- src/iface.c
+++ src/iface.c
@@ -1852,7 +1852,7 @@ IfaceInitMSS(Bund b, char *path, char *hook)
 	if (NgSendMsg(b->csock, path,
     		NGM_GENERIC_COOKIE, NGM_MKPEER, &mp, sizeof(mp)) < 0) {
     	    Log(LG_ERR, ("can't create %s node at \"%s\"->\"%s\": %s", 
-    		NG_TCPMSS_NODE_TYPE, ".", mp.ourhook, strerror(errno)));
+    		NG_TCPMSS_NODE_TYPE, path, mp.ourhook, strerror(errno)));
 	    goto fail;
 	}
 
@@ -1900,7 +1900,7 @@ IfaceInitMSS(Bund b, char *path, char *hook)
     if (NgSendMsg(b->csock, path,
 	    NGM_GENERIC_COOKIE, NGM_MKPEER, &mp, sizeof(mp)) < 0) {
     	Log(LG_ERR, ("can't create %s node at \"%s\"->\"%s\": %s", 
-    	    NG_TCPMSS_NODE_TYPE, ".", mp.ourhook, strerror(errno)));
+    	    NG_TCPMSS_NODE_TYPE, path, mp.ourhook, strerror(errno)));
 	goto fail;
     }
 
@@ -2080,7 +2080,7 @@ IfaceInitLimits(Bund b, char *path, char *hook)
 	if (NgSendMsg(b->csock, path,
 		NGM_GENERIC_COOKIE, NGM_MKPEER, &mp, sizeof(mp)) < 0) {
     	    Log(LG_ERR, ("can't create %s node at \"%s\"->\"%s\": %s", 
-    		NG_TCPMSS_NODE_TYPE, ".", mp.ourhook, strerror(errno)));
+    		NG_TCPMSS_NODE_TYPE, path, mp.ourhook, strerror(errno)));
 	    goto fail;
 	}
 
@@ -2285,7 +2285,7 @@ IfaceSetupLimits(Bund b)
 			if (NgSendMsg(b->csock, path,
 				NGM_GENERIC_COOKIE, NGM_MKPEER, &mp, sizeof(mp)) < 0) {
 		    	    Log(LG_ERR, ("[%s] IFACE: can't create %s node at \"%s\"->\"%s\": %s", 
-		    		b->name, NG_CAR_NODE_TYPE, ".", mp.ourhook, strerror(errno)));
+		    		b->name, NG_CAR_NODE_TYPE, path, mp.ourhook, strerror(errno)));
 			}
 
 		        /* Connect car to bpf. */
