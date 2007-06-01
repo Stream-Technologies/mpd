fbe273d70e615d8d381870f5c372116c2a53a231
amotin <amotin>
2007-06-01 12:19:25 +0000

    fix typo.

--- src/bund.c
+++ src/bund.c
@@ -1108,7 +1108,7 @@ BundUpdateStats(Bund b)
   struct ng_ppp_link_stat	stats;
   int	l = NG_PPP_BUNDLE_LINKNUM;
 
-#if (__FreeBSD_version < 6002104 || (__FreeBSD_version >= 700000 && __FreeBSD_version < 700029))
+#if (__FreeBSD_version < 602104 || (__FreeBSD_version >= 700000 && __FreeBSD_version < 700029))
   /* Workaround for broken ng_ppp bundle stats */
   if (!b->multilink)
     l = 0;
