06944488b60853422f866a0cc666224826e6cd04
amotin <amotin>
2007-11-01 19:10:18 +0000

    Restore LCP-Ident logging.

--- src/fsm.c
+++ src/fsm.c
@@ -1192,13 +1192,12 @@ FsmRecvDiscReq(Fsm fp, FsmHeader lhp, Mbuf bp)
 static void
 FsmRecvIdent(Fsm fp, FsmHeader lhp, Mbuf bp)
 {
-  bp = FsmCheckMagic(fp, bp);
-  if (fp->type->RecvIdent)
-    (*fp->type->RecvIdent)(fp, bp);
-  else
+    bp = FsmCheckMagic(fp, bp);
     if (bp)
-      ShowMesg(fp->log, (char *) MBDATA(bp), MBLEN(bp));
-  PFREE(bp);
+        ShowMesg(fp->log, (char *) MBDATA(bp), MBLEN(bp));
+    if (fp->type->RecvIdent)
+	(*fp->type->RecvIdent)(fp, bp);
+    PFREE(bp);
 }
 
 /*
@@ -1221,19 +1220,17 @@ FsmRecvVendor(Fsm fp, FsmHeader lhp, Mbuf bp)
 static void
 FsmRecvTimeRemain(Fsm fp, FsmHeader lhp, Mbuf bp)
 {
-  bp = FsmCheckMagic(fp, bp);
-  if (fp->type->RecvTimeRemain)
-    (*fp->type->RecvTimeRemain)(fp, bp);
-  else {
     u_int32_t	remain = 0;
 
+    bp = FsmCheckMagic(fp, bp);
     bp = mbread(bp, (u_char *) &remain, sizeof(remain), NULL);
     remain = ntohl(remain);
     Log(fp->log, (" %u seconds remain", remain));
     if (bp)
-      ShowMesg(fp->log, (char *) MBDATA(bp), MBLEN(bp));
-  }
-  PFREE(bp);
+        ShowMesg(fp->log, (char *) MBDATA(bp), MBLEN(bp));
+    if (fp->type->RecvTimeRemain)
+	(*fp->type->RecvTimeRemain)(fp, bp);
+    PFREE(bp);
 }
 
 /*
