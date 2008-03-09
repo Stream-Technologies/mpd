6420edfdba98138967691b039d60fa91eca499e7
amotin <amotin>
2008-03-09 19:16:19 +0000

    iAs soon as nothing except MD5 is supported show it like CHAP-MD5

--- src/eap.c
+++ src/eap.c
@@ -1,7 +1,7 @@
 /*
  * See ``COPYRIGHT.mpd''
  *
- * $Id: eap.c,v 1.31 2008/01/06 15:10:52 amotin Exp $
+ * $Id: eap.c,v 1.32 2008/03/09 15:27:18 amotin Exp $
  *
  */
 
@@ -182,10 +182,8 @@ EapSendRequest(Link l, u_char type)
 
   /* don't request this type again */
   eap->want_types[i] = 0;
-  a->peer_to_self_alg = req_type;
 
   switch (req_type) {
-
     case EAP_TYPE_MD5CHAL:
 
       /* Invalidate any old challenge data */
@@ -194,6 +192,7 @@ EapSendRequest(Link l, u_char type)
       chap->next_id = 1;
       chap->retry = AUTH_RETRIES;
       chap->proto = PROTO_EAP;
+      a->peer_to_self_alg = CHAP_ALG_MD5;
 
       TimerInit(&chap->chalTimer, "ChalTimer",
         l->conf.retry_timeout * SECONDS, ChapChalTimeout, l);
