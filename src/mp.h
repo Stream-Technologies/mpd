b49a5da2cc74e4996069a1a6d835b793a822ee7e
amotin <amotin>
2009-01-13 10:19:45 +0000

    Allow MRRU less then 1500 bytes. RFC claims that 1500 must be supported, but lower values are acceptable.

--- src/mp.h
+++ src/mp.h
@@ -38,7 +38,7 @@
 
 /* Bounds on things */
 
-  #define MP_MIN_MRRU		LCP_DEFAULT_MRU		/* Per RFC 1990 */
+  #define MP_MIN_MRRU		LCP_MIN_MRU		/* Per RFC 1990 */
   #define MP_MAX_MRRU		4096
   #define MP_DEFAULT_MRRU	2048
 
