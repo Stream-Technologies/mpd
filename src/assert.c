cd8c9c633aeb814019b4c2115bdc45a7bf558ded
dmitryluhtionov <dmitryluhtionov>
2015-10-29 14:14:27 +0000

    Do backtrace when assert

--- src/assert.c
+++ src/assert.c
@@ -13,8 +13,25 @@
 void
 DoAssert(const char *file, int line, const char *failedexpr)
 {
+#ifdef USE_BACKTRACE
+  void	*buffer[100];
+  char	**strings;
+  int	n, k;
+
+  n = backtrace(buffer, 100);
+  strings = backtrace_symbols(buffer, n);
+  if (strings == NULL) {
+    Log(LG_ERR, ("No backtrace symbols found"));
+  } else {
+    for (k = 0; k < n; k++) {
+      Log(LG_ERR, ("%s", strings[k]));
+    }
+    free(strings);
+  }
+#endif
   Log(LG_ERR, ("ASSERT \"%s\" failed: file \"%s\", line %d",
     failedexpr, file, line));
+
   DoExit(EX_ERRDEAD);
 }
 
